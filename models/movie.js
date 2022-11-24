ee29ba1d406a3952fe4a55499f8efb2c3cd6896b
Максим Луговской <93675697+Lugovskoy-Maxim@users.noreply.github.com>
2022-11-24 14:03:15 +0500

    add: добавил модели и ошибки

---
+++ models/movie.js
@@ -0,0 +1,70 @@
+const mongoose = require('mongoose');
+
+const urlValidator = {
+  validator(v) {
+    return /^https?:\/\/(www\.)?[0-9a-z\-._~:/?#[\]@!$&'()*+,;=]{1,}/i.test(v);
+  },
+  message: (value) => `${value} - некоректный формат ссылки`,
+};
+
+const movieSchema = new mongoose.Schema(
+  {
+    nameRU: {
+      type: String,
+      required: true,
+    },
+    nameEN: {
+      type: String,
+      required: true,
+    },
+    country: {
+      type: String,
+      required: true,
+    },
+    director: {
+      type: String,
+      required: true,
+    },
+    duration: {
+      type: Number,
+      required: true,
+    },
+    year: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      validate: urlValidator,
+      required: true,
+    },
+    trailerLink: {
+      type: String,
+      validate: urlValidator,
+      required: true,
+    },
+    thumbnail: {
+      type: String,
+      validate: urlValidator,
+      required: true,
+    },
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: 'user',
+    },
+    movieId: {
+      type: Number,
+      required: true,
+    },
+  },
+  {
+    versionKey: false,
+  },
+);
+
+module.exports = mongoose.model('user', movieSchema);
---
+++ models/users.js
@@ -0,0 +1,55 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const AuthError = require('../errors/AuthError');
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      minlength: 2,
+      maxlength: 30,
+      required: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      validate: {
+        validator(v) {
+          return /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/i.test(v);
+        },
+        message: () => 'Ошибка. Неверный формат почты',
+      },
+    },
+    password: {
+      type: String,
+      required: true,
+      select: false,
+    },
+  },
+  {
+    versionKey: false,
+  },
+);
+
+// проверка логина а после пароля (по очереди что бы не грузить сервер лишней работой)
+// eslint-disable-next-line func-names
+userSchema.statics.findUserByCredentials = function (email, password) {
+  return this.findOne({ email }).select('+password')
+    .then((user) => {
+      if (!user) {
+        throw new AuthError('Неправильные почта или пароль');
+      }
+
+      return bcrypt.compare(password, user.password)
+        .then((matched) => {
+          if (!matched) {
+            throw new AuthError('Неправильные почта или пароль');
+          }
+
+          return user;
+        });
+    });
+};
+
+module.exports = mongoose.model('user', userSchema);
