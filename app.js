19297c4b21326862365392148b5128926634d7eb
Максим Луговской <93675697+Lugovskoy-Maxim@users.noreply.github.com>
2022-11-24 12:53:12 +0500

    add: добавил комментарии

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
-const { errors } = require('celebrate');
+const { errors } = require('celebrate'); // проверка валидности ссылок и email
 
 const { PORT = 3000, MANGO_URL = 'mongodb://127.0.0.1:27017/bitfilmsdb' } = process.env; // localhost - выдеат ошибку на рабочем пк (дома проверить )
 
 const app = express();
-app.use(cookieParser());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser()); // анализирует файлЫ cookie, прикрепленных к запросу
+app.use(express.json()); // анализирует входящие запросы с полезной нагрузкой JSON
+app.use(express.urlencoded({ extended: true })); // переназначает символы которые могут нанести вред
 mongoose.connect(MANGO_URL);
 
-app.use('/*', () => { throw new Error('404 page not found'); }); // при переходе на несуществующий адрес
+app.use('/*', () => { throw new Error('this page not found'); }); // при переходе на несуществующий адрес
 app.use(errors); // обработчик ошибок валидации проверки ссылок и почты из middleware/validation
 app.use((err, req, res, next) => { // центролизованный обработчик ошибок (нужно вынести)
   const { statusCode, message } = err;
