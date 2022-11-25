70321324190a1094da494df7bed770a31c8fa078
Максим Луговской <lugovskou.myu@yandex.ru>
2022-11-25 20:54:41 +0500

    add: Добавил helmet

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
+const helmet = require('helmet');
 const { errors } = require('celebrate'); // проверка валидности ссылок и email
 
 const { PORT = 3000, MANGO_URL = 'mongodb://127.0.0.1:27017/moviesdb' } = process.env; // localhost - выдеат ошибку на рабочем пк (дома проверить ) вынести url в .env
 
 const app = express();
+app.use(helmet());
 app.use(cookieParser()); // анализирует файлЫ cookie, прикрепленных к запросу
 app.use(express.json()); // анализирует входящие запросы с полезной нагрузкой JSON
 app.use(express.urlencoded({ extended: true })); // переназначает символы которые могут нанести вред
