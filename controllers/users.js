21b25e278b54dcdc22ca6b298d92375d7d484b76
Максим Луговской <lugovskou.myu@yandex.ru>
2022-11-27 21:03:46 +0500

    fix: Исправил секретные ключи в режиме dev

--- controllers/users.js
+++ controllers/users.js
@@ -39,7 +39,7 @@ module.exports.login = (req, res, next) => {
 
   return User.findUserByCredentials(email, password)
     .then((user) => {
-      const token = jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : 'secret', { expiresIn: '7d' });
+      const token = jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : 'JWT_DEV', { expiresIn: '7d' });
       res.cookie('jwtToken', token, {
         maxAge: 3600000 * 24 * 7,
         httpOnly: true,
